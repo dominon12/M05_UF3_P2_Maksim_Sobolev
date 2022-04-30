979bd5b9885a983a20ec4c118d9029be665e9d6d
dominon12 <youneedmax@gmail.com>
2022-04-30 18:58:35 +0200

    add video page

---
+++ Pages/Products/Video.cshtml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using M05_UF3_P2_Template.App_Code.Model;
+
+namespace M05_UF3_P2_Template.Pages.Products
+{
+    public class VideoModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public long Id { get; set; }
+        [BindProperty]
+        public Video video { get; set; }
+
+        public void OnGet()
+        {
+            if (Id > 0)
+            {
+                video = new Video(Id);
+            }
+        }
+
+        public void OnPost()
+        {
+            video.Id = Id;
+            if (Id > 0)
+            {
+                video.Update();
+            }
+            else
+            {
+                video.Add();
+                Id = (long)DatabaseManager.Scalar("Video", DatabaseManager.SCALAR_TYPE.MAX, new string[] { "Id" }, "");
+                OnGet();
+            }
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            Video.Remove(id);
+            return Redirect("/Products/Searcher");
+        }
+    }
+}
